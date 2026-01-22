cafeade45a5e92ae770746804bb3e9222ed26c7d
Yugam2508 <e1092112@u.nus.edu>
2026-01-22 17:41:25 +0800

    Level-1: Echo

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Jarvis {
     public static void main(String[] args) {
         String name = "Jarvis";
@@ -9,8 +11,23 @@ public class Jarvis {
         System.out.println("What can I do for you?");
         System.out.println(horizontalLine);
 
+        Scanner scanner = new Scanner(System.in); // Create a Scanner object
+        String command = scanner.nextLine(); // Read the first line of input
+
+        // Loop: Keep reading input until the user types "bye"
+        while (!command.equals("bye")) {
+            System.out.println(horizontalLine);
+            System.out.println(command); // Echo the command
+            System.out.println(horizontalLine);
+
+            command = scanner.nextLine(); // Read the next line
+        }
+
         // Exit
+        System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
+
+        scanner.close();
     }
 }
