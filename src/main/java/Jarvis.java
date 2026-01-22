d871fedad33b898d707ea1c991075f2998cd8e8c
Yugam2508 <e1092112@u.nus.edu>
2026-01-22 18:12:38 +0800

    Level-3: Mark as Done

--- src/main/java/Jarvis.java
+++ src/main/java/Jarvis.java
@@ -5,11 +5,10 @@ public class Jarvis {
         String name = "Jarvis";
         String horizontalLine = "____________________________________________________________";
 
-        // Task storage (Simple array for Level-2)
-        String[] tasks = new String[100];
+        // Use the Task class array instead of String array
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
-        // Greet
         System.out.println(horizontalLine);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
@@ -22,13 +21,27 @@ public class Jarvis {
             System.out.println(horizontalLine);
 
             if (command.equals("list")) {
-                // Display the list
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    // Display status icon [X] or [ ]
+                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
                 }
+            } else if (command.startsWith("mark")) {
+                // Extract the number (e.g., "mark 2" -> 2)
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                tasks[index].markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + tasks[index].getStatusIcon() + "] " + tasks[index].description);
+            } else if (command.startsWith("unmark")) {
+                int index = Integer.parseInt(command.split(" ")[1]) - 1;
+                tasks[index].markAsNotDone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [" + tasks[index].getStatusIcon() + "] " + tasks[index].description);
             } else {
-                // Add to list
-                tasks[taskCount] = command;
+                // Create a new Task object
+                tasks[taskCount] = new Task(command);
                 taskCount++;
                 System.out.println("added: " + command);
             }
@@ -37,11 +50,32 @@ public class Jarvis {
             command = scanner.nextLine();
         }
 
-        // Exit
         System.out.println(horizontalLine);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(horizontalLine);
-
         scanner.close();
+    }
+}
+
+// The new Task Class (Blueprint for a task)
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
     }
 }
