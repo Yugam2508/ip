731d79f013169bb3d09c63106d0c62ef0fe380d8
Yugam2508 <e1092112@u.nus.edu>
2026-01-22 17:36:41 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Jarvis.java
@@ -0,0 +1,16 @@
+public class Jarvis {
+    public static void main(String[] args) {
+        String name = "Jarvis";
+        String horizontalLine = "____________________________________________________________";
+
+        // Greet
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontalLine);
+
+        // Exit
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
