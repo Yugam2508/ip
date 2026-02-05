0f61028e48db8004586176a13c7bbd7142dc48fe
Yugam2508 <e1092112@u.nus.edu>
2026-02-05 21:25:34 +0800

    A-Enums: Define CommandType enum for better command handling

--- src/main/java/jarvis/CommandType.java
+++ src/main/java/jarvis/CommandType.java
@@ -1,5 +1,9 @@
 package jarvis;
 
+/**
+ * Enum to represent the various commands the user can input.
+ */
+
 public enum CommandType {
     LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, FIND, BYE, UNKNOWN
 }
